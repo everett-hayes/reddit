8bbf2d12b19c49e705727679cc32a863d755f995
everett-hayes <everetty2k@gmail.com>
2021-02-14 16:25:34 -0600

    Working Post Requests and some nice modular code

--- src/script.js
+++ src/script.js
@@ -1,11 +1,5 @@
-const apiURL = 'https://polar-basin-60578.herokuapp.com/api/v1/';
-
-
-let randNum = Math.floor(Math.random() * Math.floor(5)) + 1;
-
-
-//testing the api call here
-fetch(`${apiURL}posts/${randNum}`)
-  .then(response => response.json())
-  .then(data => $('#content').append(JSON.stringify(data, null , 2)))
-
+const myPostService = new postService();
+    
+myPostService.getPosts().then((res) => {
+    $('#content').append(JSON.stringify(res, null, 2));
+})    
---
+++ src/services/postService.js
@@ -0,0 +1,42 @@
+class postService {
+
+    constructor() {
+        this.baseURL = 'https://polar-basin-60578.herokuapp.com/api/v1/posts';
+    }
+
+    getPosts() {
+        return fetch(this.baseURL)
+                .then(response => response.json())
+                .then(data => {
+                    return data;
+                })
+                .catch(error => console.warn(error));
+    }
+
+    getPostById(id) {
+        return fetch(this.baseURL + '/' + id)
+                .then(response => response.json())
+                .then(data => {
+                    return data;
+                })
+                .catch(error => console.warn(error));
+    }
+
+    createPost(content) {
+        let post =  {
+            content: content
+        }
+
+        return fetch(this.baseURL, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ post }),
+        })
+    }
+
+
+
+}
+
