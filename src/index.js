d17ddd649451b614044b6ae0f3f1abadf4c0d03a
everett-hayes <everetty2k@gmail.com>
2021-03-13 18:20:25 -0600

    yo i made a commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,34 @@ const myPostService = new postService();
 myPostService.getPosts().then((res) => {
 
     for (let item of res) {
-        $('#postTableBody').append(`<tr><td>${item.id} - ${item.content}</td></tr>`);
+        console.log(JSON.stringify(item, null, 2));
+        forPostHtml(item);
     }
 
-})  
+})
+
+function forPostHtml(post) {
+    let html = `<div id="post-${post.id}" class="post">
+                    <div id="post-voting" class="postVote">
+                        <button class="btn"><i class="fa fa-arrow-up"></i></button>
+                        <p class="voteCount">${post.vote}</p>
+                        <button class="btn"><i class="fa fa-arrow-down"></i></button>
+                    </div>
+                    <div>
+                        ${post.content}
+                    </div>
+                </div>`;
+
+    $('#postContainer').append(html);
+}
+
+{/* <div id="post-1" class="post">
+<div id="post-voting" class="postVote">
+<button class="btn"><i class="fa fa-arrow-up"></i></button>
+<p class="voteCount">4</p>
+<button class="btn"><i class="fa fa-arrow-down"></i></button>
+</div>
+<div>
+This is the content of the post
+</div>
+</div> */}
