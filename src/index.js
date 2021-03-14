2713a892389a8c00e295715191f0ecac74dc7249
everett-hayes <everetty2k@gmail.com>
2021-03-14 14:35:23 -0500

    more changes

--- src/index.js
+++ src/index.js
@@ -1,63 +1,77 @@
-const myPostService = new postService();
+'use script';
+
+let REDDIT_INDEX_PAGE = (function () {
+
+    let myPostService = new postService();
+    let posts;
+
+    function render() {
+
+        let promise = getPosts()
+            .then(renderContainer)
+        ;
+
+        return promise;
+    }
     
-myPostService.getPosts().then((res) => {
+    function getPosts() {
+        let promise = myPostService.getPosts()
+            .then((res) => {
+                posts = res;
+            })    
 
-    for (let item of res) {
-        console.log(JSON.stringify(item, null, 2));
-        renderPost(item);
+        return promise;
+    }
+    
+    function renderContainer(sortMethod) {
+
+        $('#mySpinner').hide();
+
+        if (sortMethod === 'vote') {
+            posts.sort((a,b) => (a.vote > b.vote) ? 1 : -1);
+        } else if (sortMethod === 'recent') {
+            posts.sort((a,b) => (a.created_at > b.created_at) ? 1 : -1);
+        }
+    
+        for (let post of posts) {
+            renderPost(post);
+        }
+    
     }
 
-})
-
-function render() {
-    renderContainer();
-}
-
-function renderContainer(sortMethod) {
-
-    let posts = [];
-
-    myPostService.getPosts().then((res) => {
-        posts = res;
-    })
-
-    if (sortMethod === 'vote') {
-        posts.sort((a,b) => (a.vote > b.vote) ? 1 : -1);
-    } 
-
-    for (let post of posts) {
-        renderPost(post);
+    function renderPost(post) {
+        let html = `<div id="post-${post.id}" class="post">
+                        <div id="post-voting" class="postVote">
+                            <button class="btn" onclick="REDDIT_INDEX_PAGE.vote(${post.id}, 'up');"><i class="fa fa-arrow-up"></i></button>
+                            <p id="post-${post.id}-vote" class="voteCount">${post.vote}</p>
+                            <button class="btn" onclick="REDDIT_INDEX_PAGE.vote(${post.id}, 'down');"><i class="fa fa-arrow-down"></i></button>
+                        </div>
+                        <div id="post-${post.id}-content">
+                            ${post.content}
+                        </div>
+                    </div>`;
+    
+        $('#postContainer').append(html);
+    }
+    
+    function vote(index, direction) {
+    
+        let count = parseInt($(`#post-${index}-vote`).html());
+    
+        (direction === 'up') ? count++ : count--;
+    
+        post = {
+            content : $(`#post-${index}-content`).html(),
+            vote: count
+        }
+    
+        //myPostService.updatePostById(index, post);
+        $(`#post-${index}-vote`).html(count);
     }
 
-}
-
-function vote(index, direction) {
-
-    let count = parseInt($(`#post-${index}-vote`).html());
-
-    (direction === 'up') ? count++ : count--;
-
-    post = {
-        content : $(`#post-${index}-content`).html(),
-        vote: count
+    return {
+        render : render,
+        vote : vote
     }
 
-    console.log(post);
-    //myPostService.updatePostById(index, post);
-    $(`#post-${index}-vote`).html(count);
-}
-
-function renderPost(post) {
-    let html = `<div id="post-${post.id}" class="post">
-                    <div id="post-voting" class="postVote">
-                        <button class="btn" onclick="vote(${post.id}, 'up');"><i class="fa fa-arrow-up"></i></button>
-                        <p id="post-${post.id}-vote" class="voteCount">${post.vote}</p>
-                        <button class="btn" onclick="vote(${post.id}, 'down');"><i class="fa fa-arrow-down"></i></button>
-                    </div>
-                    <div id="post-${post.id}-content">
-                        ${post.content}
-                    </div>
-                </div>`;
-
-    $('#postContainer').append(html);
-}
+}());
